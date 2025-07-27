d2f079d76b91c6c49fcd8164bf359df7bed933c7
Nahom Tigistu <nahomtigistu777@gmail.com>
2025-07-27 13:16:50 +0300

    Backend deployed on render, api url changed for the frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL =
-  'https://song-discovery-by-nahom-9e5e4e24a578.herokuapp.com/api';
+const BASE_URL = 'https://songify-etba.onrender.com/api';
 const api = axios.create({
   baseURL: BASE_URL,
 });
